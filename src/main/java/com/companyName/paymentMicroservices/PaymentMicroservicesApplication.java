c22af907deb94132df61d1ce05bb030945149965
Marino.Malinconico <marino.malinconico@nttdata.com>
2025-11-14 12:11:58 +0100

    changed configuration to make the db permanent

--- src/main/java/com/companyName/paymentMicroservices/PaymentMicroservicesApplication.java
+++ src/main/java/com/companyName/paymentMicroservices/PaymentMicroservicesApplication.java
@@ -27,10 +27,10 @@ public class PaymentMicroservicesApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         log.info("Metodo run");
                                                                     //YYYY-mm-dd 2025-09-30
-        paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"pippo",    "RGNRDV87H13D761R", new BigDecimal("3000.01"), "EUR"));
-        paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"pluto",   "RGNLSN87H13D761R", new BigDecimal("3000.02"), "JPY"));
-        paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"paperino","FRNFBA85M08D761M", new BigDecimal("3000.03"), "USD"));
-        paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"topolino","DSTLCU89R52D761R", new BigDecimal("3000.04"), "GBP"));
-        paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"sora",    "DSTLCU89R52D761R", new BigDecimal("3000.05"), "RUB"));
+        //paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"pippo",   "RGNRDV87H13D761R", new BigDecimal("3000.01"), "EUR"));
+        //paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"pluto",   "RGNLSN87H13D761R", new BigDecimal("3000.02"), "JPY"));
+        //paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"paperino","FRNFBA85M08D761M", new BigDecimal("3000.03"), "USD"));
+        //paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"topolino","DSTLCU89R52D761R", new BigDecimal("3000.04"), "GBP"));
+        //paymentRepository.save(new Payment(null, LocalDate.parse("2024-09-22"),"sora",    "DSTLCU89R52D761R", new BigDecimal("3000.05"), "RUB"));
     }
 }
